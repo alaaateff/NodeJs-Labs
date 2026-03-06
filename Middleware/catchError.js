530714a29daa9b446ab2bbb8a8e22361d616a3a9
alaaateff <alaaatef3200@gmail.com>
2026-03-06 16:20:25 +0200

    Remove all files from main

--- Middleware/catchError.js
+++
@@ -1,7 +0,0 @@
-export default function catchError(Func){
-    return(req,res) => {
-        Func(req,res).catch((err) => {
-            res.status(500).json({message: err})
-        })
-    }
-}
--- Middleware/hashPassword.js
+++
@@ -1,9 +0,0 @@
-
-import bcrypt from "bcrypt"
-
-let hashPassword = (req,res,next) => {
-    req.body.password = bcrypt.hashSync(req.body.password, 8)
-    next();
-}
-
-export default hashPassword
